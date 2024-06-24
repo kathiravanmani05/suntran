2cb4356775eeaa422251721a70000d56b0aabbab
sudhakar6295 <53999168+sudhakar6295@users.noreply.github.com>
2024-06-24 17:36:25 +0530

    Update suntransfer_price1.py

--- suntransfers/spiders/suntransfer_price1.py
+++ suntransfers/spiders/suntransfer_price1.py
@@ -77,7 +77,7 @@ class SuntransferPriceSpider(scrapy.Spider):
         df = pd.read_excel('Batch2_input3.xlsx')
         
         tasks = []
-        for i in range(2800, 3300, 10):  # Process rows in batches of 10
+        for i in range(3300, 8000, 10):  # Process rows in batches of 10
             batch = df.iloc[i:i+10]
             tasks.append(self.process_batch(batch))
         
